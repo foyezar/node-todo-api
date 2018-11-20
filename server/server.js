82ad02dc0dcf2ef87ff23f7d5b1a23b6d848a4b9
Foyez <foyezar@gmail.com>
2018-11-20 18:44:24 +0600

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 const express      = require('express'),
       bodyParser   = require('body-parser')
 
-const { mongoose } = require('./db/mongoose'),
+const { ObjectID } = require('mongodb'),
+      { mongoose } = require('./db/mongoose'),
       { Todo }     = require('./models/todo'),
       { User }     = require('./models/user')
 
@@ -25,6 +26,22 @@ app.get('/todos', (req, res) => {
   Todo.find().then(todos => {
     res.send({ todos })
   }).catch(e => res.status(400).send(e))
+})
+
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id
+  
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send()
+  }
+
+  Todo.findById(id).then(todo => {
+    if (!todo) {
+      return res.status(404).send
+    }
+
+    res.send({ todo })
+  }).catch(e => res.status(400).send())
 })
 
 app.listen(3000, () => console.log('Server has started'))
